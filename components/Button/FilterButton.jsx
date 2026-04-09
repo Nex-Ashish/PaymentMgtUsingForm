68edde8260073e81d82857650f1c05323cf9cfb5
Ashish Panwar <ashish0410@nexzem.com>
2026-04-09 15:45:09 +0530

    submission filter completed

---
+++ components/Button/FilterButton.jsx
@@ -0,0 +1,14 @@
+export default function FilterButton({ value, onChange }) {
+    return (
+        <div className="flex gap-1 items-center ">
+
+            <h2 className="text-center text-xl">Filter</h2>
+
+            <select value={value} onChange={(e) => onChange(e?.target?.value)} name="status" className="p-2 my-3 border rounded-2xl cursor-pointer bg-blue-100 w-sm self-center">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="pending">Pending</option>
+            </select>
+        </div>
+    )
+}
--- components/MockData/MockData.js
+++ components/MockData/MockData.js
@@ -3,30 +3,35 @@ export const MockData = [
     name: "Ashish Panwar",
     email: "ashish@gmail.com",
     text: "Payment for MCA course",
-    amount: "5000"
+    amount: "5000",
+    status: "Pending",
   },
   {
     name: "Rahul Sharma",
     email: "rahul@gmail.com",
     text: "BCA admission fee",
-    amount: "3000"
+    amount: "3000",
+    status: "Completed"
   },
   {
     name: "Priya Singh",
     email: "priya@gmail.com",
     text: "MBA registration",
-    amount: "7000"
+    amount: "7000",
+    status: "Completed"
   },
   {
     name: "Amit Verma",
     email: "amit@gmail.com",
     text: "B.Tech semester fee",
-    amount: "10000"
+    amount: "10000",
+    status: "Completed"
   },
   {
     name: "Neha Gupta",
     email: "neha@gmail.com",
     text: "MCA project fee",
-    amount: "4500"
+    amount: "4500",
+    status: "Pending"
   }
 ];
